6bdb2960515a7e989fca2ee262cca07a031be18f
Nahida-Chowdhury <chowdhurynahida06@gmail.com>
2026-07-19 16:53:13 +0600

    feat: add floating scroll-to-top button

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,36 @@
 <template>
-  <router-view />
+  <RouterView />
+
+  <button
+    v-show="showScrollTop"
+    class="scroll-top-btn"
+    @click="scrollToTop"
+  >
+    <i class="bi bi-arrow-up"></i>
+  </button>
 </template>
 
 <script setup>
+import { ref, onMounted, onBeforeUnmount } from "vue";
+
+const showScrollTop = ref(false);
+
+function handleScroll() {
+  showScrollTop.value = window.scrollY > 300;
+}
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
 </script>
